4889c6c762eb57275b9625c56f8f0b9f8424d4dc
SunOner <44489132+SunOner@users.noreply.github.com>
2025-06-07 11:29:46 +0300

    Update kmboxNetConnection.cpp

--- sunone_aimbot_cpp/mouse/kmboxNetConnection.cpp
+++ sunone_aimbot_cpp/mouse/kmboxNetConnection.cpp
@@ -55,7 +55,7 @@ void KmboxNetConnection::monitorThread()
             shooting.store(false);
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
